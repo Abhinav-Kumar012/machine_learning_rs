3ba614da506929c388c8cb3ee2910797134a1fad
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-05-11 15:10:47 +0530

    parsed test data - 1

--- dataset_processing/src/main.rs
+++ dataset_processing/src/main.rs
@@ -1,18 +1,52 @@
 use std::{error::Error, fs::File};
 
-use csv::Reader;
+use csv::ReaderBuilder;
+use serde::Deserializer;
 
 const DSET_PATH: &str = "data/titanic";
+
+#[derive(Debug, serde::Deserialize)]
+struct RawRecord {
+	#[serde(rename(deserialize = "PassengerId"))]
+	rec_no: u32,
+	#[serde(rename(deserialize = "Survived"))]
+	survived: u8,
+	#[serde(rename(deserialize = "Pclass"))]
+	passenger_class: u8,
+	#[serde(rename(deserialize = "Name"))]
+	name: String,
+	#[serde(rename(deserialize = "Sex"))]
+	sex: String,
+	#[serde(rename(deserialize = "Age"))]
+	age: Option<f32>,
+	#[serde(rename(deserialize = "SibSp"))]
+	sib_sp: u8,
+	#[serde(rename(deserialize = "Parch"))]
+	parch: u8,
+	#[serde(rename(deserialize = "Ticket"))]
+	ticket: String,
+	#[serde(rename(deserialize = "Fare"))]
+	fare: f64,
+	#[serde(rename(deserialize = "Cabin"))]
+	cabin: String,
+	#[serde(rename(deserialize = "Embarked"))]
+	embark: Option<char>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let file_path = format!("{DSET_PATH}/train.csv");
 
 	let fd = File::open(file_path)?;
 
-	let mut reader = Reader::from_reader(fd);
+	let mut reader = ReaderBuilder::new()
+		.delimiter(b',')
+		.has_headers(true)
+		.quote(b'\"')
+		.from_reader(fd);
 
-	for result in reader.records() {
-		println!("{:?}", result?);
+	for result in reader.deserialize() {
+		let record: RawRecord = result?;
+		println!("{record:?}");
 	}
 	Ok(())
 }
