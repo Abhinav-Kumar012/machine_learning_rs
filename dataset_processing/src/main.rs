ac358e2b315151cac25221fbac52bdd9bd179691
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-05-11 15:31:36 +0530

    refined struct definition

--- dataset_processing/src/main.rs
+++ dataset_processing/src/main.rs
@@ -1,22 +1,41 @@
 use std::{error::Error, fs::File};
 
 use csv::ReaderBuilder;
-use serde::Deserializer;
 
 const DSET_PATH: &str = "data/titanic";
 
 #[derive(Debug, serde::Deserialize)]
+enum sex {
+	#[serde(rename = "male")]
+	male,
+	#[serde(rename = "female")]
+	female,
+}
+
+#[derive(Debug, serde::Deserialize)]
+enum port {
+	#[serde(rename = "C")]
+	Cherbourg,
+	#[serde(rename = "Q")]
+	Queenstown,
+	#[serde(rename = "S")]
+	Southampton,
+	#[serde(other)]
+	Missing,
+}
+
+#[derive(Debug, serde::Deserialize)]
 struct RawRecord {
-	#[serde(rename(deserialize = "PassengerId"))]
-	rec_no: u32,
+	// #[serde(rename(deserialize = "PassengerId"))]
+	// rec_no: u32,
 	#[serde(rename(deserialize = "Survived"))]
 	survived: u8,
 	#[serde(rename(deserialize = "Pclass"))]
 	passenger_class: u8,
-	#[serde(rename(deserialize = "Name"))]
-	name: String,
+	// #[serde(rename(deserialize = "Name"))]
+	// name: String,
 	#[serde(rename(deserialize = "Sex"))]
-	sex: String,
+	sex: sex,
 	#[serde(rename(deserialize = "Age"))]
 	age: Option<f32>,
 	#[serde(rename(deserialize = "SibSp"))]
@@ -27,10 +46,10 @@ struct RawRecord {
 	ticket: String,
 	#[serde(rename(deserialize = "Fare"))]
 	fare: f64,
-	#[serde(rename(deserialize = "Cabin"))]
-	cabin: String,
+	// #[serde(rename(deserialize = "Cabin"))]
+	// cabin: String,
 	#[serde(rename(deserialize = "Embarked"))]
-	embark: Option<char>,
+	embark: port,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
